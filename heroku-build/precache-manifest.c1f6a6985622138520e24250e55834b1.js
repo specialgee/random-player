8f2834ef85cdfb84dd0fe17b7cc75b13d0a2c1be
gee <special.geeee@gmail.com>
2020-04-05 02:29:39 +0200

    update

---
+++ heroku-build/precache-manifest.c1f6a6985622138520e24250e55834b1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e57456eb9af0b9dc9998ccf575ea4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ec70b244c8e1b0a7390",
+    "url": "/static/css/2.fbd4d1d5.chunk.css"
+  },
+  {
+    "revision": "303736bf2e75817ac033",
+    "url": "/static/css/main.1d3014a8.chunk.css"
+  },
+  {
+    "revision": "2ec70b244c8e1b0a7390",
+    "url": "/static/js/2.2edebf66.chunk.js"
+  },
+  {
+    "revision": "de868db200efedf7e7c9f76db7ee3ad9",
+    "url": "/static/js/2.2edebf66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "303736bf2e75817ac033",
+    "url": "/static/js/main.ed0ec628.chunk.js"
+  },
+  {
+    "revision": "ff694394acffeacbe82d",
+    "url": "/static/js/runtime-main.b2b29c3d.js"
+  },
+  {
+    "revision": "b540bd7da4e1619b976b8ffce9eaee6c",
+    "url": "/static/media/button-rap-active.b540bd7d.jpg"
+  },
+  {
+    "revision": "9a0d2607b54fdd74d34a0d94f58b60cd",
+    "url": "/static/media/quarantine-logo.9a0d2607.gif"
+  }
+]);
