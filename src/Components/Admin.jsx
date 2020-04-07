65a427c9c05aec0c34fe78fd04ebffb1582703f7
gee <special.geeee@gmail.com>
2020-04-07 06:04:43 +0200

    added construction stripes on Admin page

--- src/Components/Admin.jsx
+++ src/Components/Admin.jsx
@@ -9,6 +9,10 @@ import './Admin.css';
 function Admin() {
     return (
         <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <div className="d-flex justify-content-between">
+                <div className="stripes"></div>
+                <div className="stripes stripes-right"></div>
+            </div>
             <NavBar />
             <Switch>
                 <Route path="/admin/videos/list"  component={VideoList} />
