f7ca00d4f969c94d088aa5de8affaf2ddd639db5
gee <special.geeee@gmail.com>
2020-04-09 14:50:41 +0200

    added video API baseUrl

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'localhost:3000/api',
+    baseURL:  process.env.REACT_APP_SERVER_URL + '/api',
 })
 
 export const insertVideo = payload => api.post(`/video`, payload);
