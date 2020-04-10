5ff61b768045f5151db55486f211cb36d5b1faf0
gee <special.geeee@gmail.com>
2020-04-10 18:34:45 +0200

    :sparkles:

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -17,10 +17,6 @@ class LoginPage extends React.Component {
     render() {
         return (
             <div>
-                {/* <div className="alert alert-info">
-                    <strong>Normal User</strong> - U: user P: user<br />
-                    <strong>Administrator</strong> - U: admin P: admin
-                </div> */}
                 <h2>Login</h2>
                 <Formik
                     initialValues={{
