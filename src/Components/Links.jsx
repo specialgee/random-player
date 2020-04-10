a7a6397ecd1dbd09952d63d75b785910f9b54afe
gee <special.geeee@gmail.com>
2020-04-10 18:37:25 +0200

    :sparkles:

--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -1,64 +1,15 @@
 import React, { Component } from 'react';
-//import { Link } from 'react-router-dom';
-//import styled from 'styled-components';
-//import { history } from '../helpers';
 import { authenticationService } from '../services';
 import { Nav, Button } from 'react-bootstrap';
-
-
-// const Collapse = styled.div.attrs({
-//     className: 'navbar-collapse',
-// })``
-
-// const List = styled.div.attrs({
-//     className: 'navbar-nav',
-// })``
-
-// const Item = styled.div.attrs({
-//     className: 'navbar-collapse',
-// })``
 
 class Links extends Component {
     
     logout() {
         authenticationService.logout();
-        //history.push('/login');
     }
   
     render() {
         return (
-            // <React.Fragment>
-            //     <Link to="/admin" className="navbar-brand">
-            //         ADMIN
-            //     </Link>
-            //     <Collapse>
-            //         <List  id="collapseExample" className="mr-auto">
-            //             <Item>
-            //                 <a href="/" className="nav-link">
-            //                     HOME
-            //                 </a>
-            //             </Item>
-            //             <Item>
-            //                 <Link to="/admin/videos/list" className="nav-link">
-            //                     LIST
-            //                 </Link>
-            //             </Item>
-            //             <Item>
-            //                 <Link to="/admin/videos/create" className="nav-link">
-            //                     ADD VIDEO
-            //                 </Link>
-            //             </Item>
-            //         </List>
-            //         <List className="navbar-right ml-auto">
-            //             <Item>
-            //                 <Link to="/login" className="nav-link" onClick={this.logout}>
-            //                     LOGOUT
-            //                 </Link>
-            //             </Item>
-            //         </List>
-            //     </Collapse>
-            // </React.Fragment>
-
             <React.Fragment>
                 <Nav className="mr-auto">
                 <Nav.Link className="nav-link" href="/">HOME</Nav.Link>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,33 +1,10 @@
 import React, { Component } from 'react';
-//import styled from 'styled-components';
-
 import Links from './Links';
-
 import { Navbar } from 'react-bootstrap';
-
-// const Container = styled.div.attrs({
-//     className: 'admin-container',
-// })`
-//     height: 150px;
-// `
-
-// const Nav = styled.nav.attrs({
-//     className: 'navbar navbar-expand-lg navbar-dark',
-// })`
-//     margin-bottom: 20 px;
-//     background-color: #000;
-//     border-bottom: 1px solid #fff;
-// `
 
 class NavBar extends Component {
     render() {
         return (
-            // <Container>
-            //     <Nav>
-            //         <Links />
-            //     </Nav>
-            // </Container>
-
             <Navbar className="admin-container" expand="lg">
             <Navbar.Brand className="ml-4" href="/admin">ADMIN</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
